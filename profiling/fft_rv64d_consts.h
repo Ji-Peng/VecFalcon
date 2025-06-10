eb2aef565badd4fd70f305f0f53a780db406112a
jipengzhang <jp-zhang@outlook.com>
2025-06-10 10:10:04 +0800

    optimized implementation of FFT/iFFT on RV64D

---
+++ profiling/fft_rv64d_consts.h
@@ -0,0 +1,34 @@
+#ifndef FFT_RV64D_CONSTS_H
+
+#define GM332_l4_IDX 14
+#define GM332_l5_IDX 16
+#define GM332_l6_IDX 20
+#define GM332_l4_NUM 2
+#define GM332_l5_NUM 4
+#define GM332_l6_NUM 8
+#define GM332_l4_5_NUM 6
+#define GM332_l4_6_NUM 14
+#define GM332_l7_IDX 126
+#define GM332_l8_IDX 130
+#define GM332_l7_NUM 4
+#define GM332_l8_NUM 8
+#define GM332_l7_8_NUM 12
+
+#define GM333_l4_IDX 14
+#define GM333_l5_IDX 16
+#define GM333_l6_IDX 20
+#define GM333_l4_NUM 2
+#define GM333_l5_NUM 4
+#define GM333_l6_NUM 8
+#define GM333_l4_5_NUM 6
+#define GM333_l4_6_NUM 14
+#define GM333_l7_IDX 126
+#define GM333_l8_IDX 128
+#define GM333_l9_IDX 132
+#define GM333_l7_NUM 2
+#define GM333_l8_NUM 4
+#define GM333_l9_NUM 8
+#define GM333_l7_8_NUM 6
+#define GM333_l7_9_NUM 14
+
+#endif
