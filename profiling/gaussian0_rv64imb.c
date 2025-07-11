54f210a98bf02fb88943a11583d433acfdf01af6
jipengzhang <jp-zhang@outlook.com>
2025-07-11 16:32:58 +0800

    update

--- profiling/gaussian0_rv64imb.c
+++ profiling/gaussian0_rv64imb.c
@@ -320,99 +320,6 @@ void gaussian0_rv64im_64w(sampler_state *ss, int32_t *z_bimodal,
     }
 }
 
-typedef union {
-    uint32_t u32[3][8] __attribute__((aligned(32)));
-} prn_24x3_8w;
-
-#define ALIGNED_INT32(N)   \
-    union {                \
-        int32_t coeffs[N]; \
-    }
-
-extern void gaussian0_rvv(int32_t *out, uint32_t *prn, size_t n);
-
-void gaussian0_rvv_16w(sampler_state *ss, int32_t *z_bimodal,
-                       int32_t *z_square)
-{
-    prn_24x3_8w prn[2];
-    int32_t z[16] __attribute__((aligned(32)));
-    int32_t *_z_bi = (int32_t *)z_bimodal;
-    int32_t *_z_sq = (int32_t *)z_square;
-
-    for (int j = 0; j < 2; j++)
-        for (int i = 0; i < 8; i++) {
-            prn[j].u32[0][i] = prng_next_u24(&ss->pc);
-            prn[j].u32[1][i] = prng_next_u24(&ss->pc);
-            prn[j].u32[2][i] = prng_next_u24(&ss->pc);
-        }
-    gaussian0_rvv(z, &prn[0].u32[0][0], 2);
-    uint16_t b_16b = prng_next_u16(&ss->pc);
-    for (size_t j = 0; j < 16; j += 2) {
-        int32_t b0 = (b_16b >> j) & 1;
-        int32_t b1 = (b_16b >> (j + 1)) & 1;
-        int32_t m0 = (b0 << 1);
-        int32_t m1 = (b1 << 1);
-        m0 = m0 - 1;
-        m1 = m1 - 1;
-        m0 = m0 * z[j];
-        m1 = m1 * z[j + 1];
-        _z_bi[j] = b0 + m0;
-        _z_bi[j + 1] = b1 + m1;
-        _z_sq[j] = z[j] * z[j];
-        _z_sq[j + 1] = z[j + 1] * z[j + 1];
-    }
-}
-
-extern void gaussian0_rvv_bisq(int32_t *z_bimodal, int32_t *z_square,
-                               uint32_t *prn, uint32_t *b_16bs, size_t n);
-// including the bimodal tunning and square calculation
-void gaussian0_bisq_rvv_16w(sampler_state *ss, int32_t *z_bimodal,
-                            int32_t *z_square)
-{
-    prn_24x3_8w prn[2];
-    uint32_t b_16bs[16];
-    int32_t *_z_bi = (int32_t *)z_bimodal;
-    int32_t *_z_sq = (int32_t *)z_square;
-
-    for (int j = 0; j < 2; j++)
-        for (int i = 0; i < 8; i++) {
-            prn[j].u32[0][i] = prng_next_u24(&ss->pc);
-            prn[j].u32[1][i] = prng_next_u24(&ss->pc);
-            prn[j].u32[2][i] = prng_next_u24(&ss->pc);
-        }
-    uint16_t b_16b = prng_next_u16(&ss->pc);
-    for (int i = 0; i < 16; i += 2) {
-        b_16bs[i] = (b_16b >> i) & 1;
-        b_16bs[i + 1] = (b_16b >> (i + 1)) & 1;
-    }
-    gaussian0_rvv_bisq(_z_bi, _z_sq, &prn[0].u32[0][0], b_16bs, 2);
-}
-
-// including the bimodal tunning and square calculation
-void gaussian0_bisq_rvv_64w(sampler_state *ss, int32_t *z_bimodal,
-                            int32_t *z_square)
-{
-    ALIGNED_INT32(64) b_64bs;
-    prn_24x3_8w prn[8];
-    int32_t *_z_bi = (int32_t *)z_bimodal;
-    int32_t *_z_sq = (int32_t *)z_square;
-
-    for (int j = 0; j < 8; j++)
-        for (int i = 0; i < 8; i++) {
-            prn[j].u32[0][i] = prng_next_u24(&ss->pc);
-            prn[j].u32[1][i] = prng_next_u24(&ss->pc);
-            prn[j].u32[2][i] = prng_next_u24(&ss->pc);
-        }
-    uint64_t b_64b = prng_next_u64(&ss->pc);
-    for (int i = 0; i < 64; i += 4) {
-        b_64bs.coeffs[i] = (b_64b >> i) & 1;
-        b_64bs.coeffs[i + 1] = (b_64b >> (i + 1)) & 1;
-        b_64bs.coeffs[i + 2] = (b_64b >> (i + 2)) & 1;
-        b_64bs.coeffs[i + 3] = (b_64b >> (i + 3)) & 1;
-    }
-    gaussian0_rvv_bisq(_z_bi, _z_sq, &prn[0].u32[0][0], b_64bs.coeffs, 8);
-}
-
 /**
  * Only used to test the performance of core calculations, excluding the
  * overhead caused by obtaining random numbers.
@@ -443,24 +350,16 @@ void gaussian0_rv64im_nw_core(int32_t *z_bimodal, int32_t *z_square,
     }
 }
 
-/**
- * Only used to test the performance of core calculations, excluding the
- * overhead caused by obtaining random numbers.
- */
-void gaussian0_rvv_nw_core(int32_t *z_bimodal, int32_t *z_square, size_t n)
-{
-    int32_t *_z_bi = z_bimodal;
-    int32_t *_z_sq = z_square;
-    uint32_t t[128] __attribute__((aligned(32)));
-
-    gaussian0_rvv_bisq(_z_bi, _z_sq, t, t, n >> 3);
-}
-
 #define WARMUP_N 1000
 // 136*4-(136*4%3)=543
 // 543*3 % 9 = 0
 #define TESTS_N ((543 * 3) * 100)
 #define SAMPLES_N (8 * (1 << 5))
+
+#define ALIGNED_INT32(N)   \
+    union {                \
+        int32_t coeffs[N]; \
+    }
 
 void test_gaussian0()
 {
@@ -510,37 +409,6 @@ void test_gaussian0()
     memset(&z0_bi, 0, sizeof(z0_bi));
     memset(&z0_sq, 0, sizeof(z0_sq));
     sampler_init(&ss0, 9, seed, 32);
-    for (i = 0; i < SAMPLES_N; i += 16) {
-        gaussian0_ref_16w_u24(&ss0, &z0_bi.coeffs[i], &z0_sq.coeffs[i]);
-    }
-    memset(&z1_bi, 0, sizeof(z1_bi));
-    memset(&z1_sq, 0, sizeof(z1_sq));
-    sampler_init(&ss1, 9, seed, 32);
-    for (i = 0; i < SAMPLES_N; i += 16) {
-        gaussian0_rvv_16w(&ss1, &z1_bi.coeffs[i], &z1_sq.coeffs[i]);
-    }
-    if (memcmp(z0_bi.coeffs, z1_bi.coeffs, sizeof(int32_t) * SAMPLES_N) !=
-            0 ||
-        memcmp(z0_sq.coeffs, z1_sq.coeffs, sizeof(int32_t) * SAMPLES_N) !=
-            0) {
-        printf("gaussian0_ref_16w_u24 != gaussian0_rvv_16w\n");
-    }
-    memset(&z1_bi, 0, sizeof(z1_bi));
-    memset(&z1_sq, 0, sizeof(z1_sq));
-    sampler_init(&ss1, 9, seed, 32);
-    for (i = 0; i < SAMPLES_N; i += 16) {
-        gaussian0_bisq_rvv_16w(&ss1, &z1_bi.coeffs[i], &z1_sq.coeffs[i]);
-    }
-    if (memcmp(z0_bi.coeffs, z1_bi.coeffs, sizeof(int32_t) * SAMPLES_N) !=
-            0 ||
-        memcmp(z0_sq.coeffs, z1_sq.coeffs, sizeof(int32_t) * SAMPLES_N) !=
-            0) {
-        printf("gaussian0_ref_16w_u24 != gaussian0_bisq_rvv_16w\n");
-    }
-
-    memset(&z0_bi, 0, sizeof(z0_bi));
-    memset(&z0_sq, 0, sizeof(z0_sq));
-    sampler_init(&ss0, 9, seed, 32);
     for (i = 0; i < SAMPLES_N; i += 64) {
         gaussian0_ref_64w(&ss0, &z0_bi.coeffs[i], &z0_sq.coeffs[i]);
     }
@@ -557,17 +425,6 @@ void test_gaussian0()
         printf("gaussian0_ref_64w != gaussian0_rv64im_64w\n");
     }
 }
-extern void init_vector_e16();
-extern void init_vector_e32();
-extern void init_vector_e64();
-extern void gaussian0_rvv_24b_p0_x2();
-extern void gaussian0_rvv_24b_p0_x3();
-extern void gaussian0_rvv_24b_p1_x3();
-extern void gaussian0_rvv_24b_p1_x3_v2();
-extern void gaussian0_rvv_64b_p0_x2();
-extern void gaussian0_rvv_64b_p0_x3();
-extern void gaussian0_rvv_64b_p1_x3();
-extern void gaussian0_rvv_64b_p1_x3_v2();
 
 void speed_gaussian0()
 {
@@ -590,14 +447,6 @@ void speed_gaussian0()
     PERF_N(gaussian0_rv64im_64w(&ss0, &z0_bi.coeffs[0], &z0_sq.coeffs[0]),
            gaussian0_rv64im_64w, sampler_init(&ss0, 9, seed, 32), WARMUP_N,
            TESTS_N, 64);
-    PERF_N(
-        gaussian0_bisq_rvv_16w(&ss0, &z0_bi.coeffs[0], &z0_sq.coeffs[0]),
-        gaussian0_bisq_rvv_16w, sampler_init(&ss0, 9, seed, 32), WARMUP_N,
-        TESTS_N, 16);
-    PERF_N(
-        gaussian0_bisq_rvv_64w(&ss0, &z0_bi.coeffs[0], &z0_sq.coeffs[0]),
-        gaussian0_bisq_rvv_64w, sampler_init(&ss0, 9, seed, 32), WARMUP_N,
-        TESTS_N, 64);
     /**
      * Test the performance of core calculations, excluding
      * the overhead caused by obtaining random numbers.
@@ -616,35 +465,6 @@ void speed_gaussian0()
         gaussian0_rv64im_nw_core(&z0_bi.coeffs[0], &z0_sq.coeffs[0], 64),
         gaussian0_rv64im_64w_core, sampler_init(&ss0, 9, seed, 32),
         WARMUP_N, TESTS_N, 64);
-    PERF_N(gaussian0_rvv_nw_core(&z0_bi.coeffs[0], &z0_sq.coeffs[0], 16),
-           gaussian0_rvv_16w_core, sampler_init(&ss0, 9, seed, 32),
-           WARMUP_N, TESTS_N, 16);
-    PERF_N(gaussian0_rvv_nw_core(&z0_bi.coeffs[0], &z0_sq.coeffs[0], 64),
-           gaussian0_rvv_64w_core, sampler_init(&ss0, 9, seed, 32),
-           WARMUP_N, TESTS_N, 64);
-
-#if REF_SHA3 == 0
-    printf("\nSome optimization ideas:\n");
-    init_vector_e64();
-    PERF_N(gaussian0_rvv_64b_p0_x2(), gaussian0_rvv_64b_p0_x2, , WARMUP_N,
-           TESTS_N, 2);
-    PERF_N(gaussian0_rvv_64b_p0_x3(), gaussian0_rvv_64b_p0_x3, , WARMUP_N,
-           TESTS_N, 3);
-    PERF_N(gaussian0_rvv_64b_p1_x3(), gaussian0_rvv_64b_p1_x3, , WARMUP_N,
-           TESTS_N, 3);
-    PERF_N(gaussian0_rvv_64b_p1_x3_v2(), gaussian0_rvv_64b_p1_x3_v2, ,
-           WARMUP_N, TESTS_N, 3);
-
-    init_vector_e32();
-    PERF_N(gaussian0_rvv_24b_p0_x2(), gaussian0_rvv_24b_p0_x2, , WARMUP_N,
-           TESTS_N, 2);
-    PERF_N(gaussian0_rvv_24b_p0_x3(), gaussian0_rvv_24b_p0_x3, , WARMUP_N,
-           TESTS_N, 3);
-    PERF_N(gaussian0_rvv_24b_p1_x3(), gaussian0_rvv_24b_p1_x3, , WARMUP_N,
-           TESTS_N, 3);
-    PERF_N(gaussian0_rvv_24b_p1_x3_v2(), gaussian0_rvv_24b_p1_x3_v2, ,
-           WARMUP_N, TESTS_N, 3);
-#endif
 }
 
 int main(void)
